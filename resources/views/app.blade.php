c4c465969bd2b42e06a1221a1c4844049e3eabe5
HP <muhammadaniqaiman00@gmail.com>
2023-05-25 18:56:02 +0800

    First commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My App</title>
+    </head>
+    <body>
+        @include('navbar')
+        
+        <div class="container">
+            @yield('content')
+            
+        </div>
+    </body>
+</html>
